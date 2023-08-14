780ce5415509d0acaf332bd41a687c345ab19397
Henry Lopez <34701402+HenrySD@users.noreply.github.com>
2023-08-14 13:39:08 -0600

    Add files via upload

---
+++ Ejercicios.py
@@ -0,0 +1,25 @@
+MiNombre = "Henry Lopez"
+MiEdad = 26
+
+print(f"Mi Nombre es: {MiNombre} y mi edad es de: {MiEdad}")
+print(f"Los caracteres de mi nombre son: {len(MiNombre)}")
+
+
+if MiEdad > 18:
+    print('eres mayor de edad')
+    if MiEdad % 2 == 0:
+        print('tu edad es Par')
+    else:
+        print('tu edad no es impar')
+else:
+    print('no eres mayor de edad')
+
+
+"""dasdasdaddasdadadadadaddasdada
+hola comoe stan a todos
+jajajaj este es un comentario multi linea verdad
+dadadad
+"""
+x, y, z = 2, 23.7, 'sabdia'
+
+print(x+y+z)
